1a5e40dc1ccf08fa14ee893841c4628e2ad1aaab
Sumeet Balwade <balwadesr@gmail.com>
2021-05-13 17:05:18 +0530

    best places done

--- resources/views/components/bestplace.blade.php
+++ resources/views/components/bestplace.blade.php
@@ -4,45 +4,55 @@
             <span>Best Places to checkout</span></h3>
         <div class="row d-flex align-items-center justify-content-center">
 
-            @for ($i=0;$i<4;$i++)
+            @php
+                $bplaces = App\Models\Property::orderBy('ratings','desc')->take(4)->get();
+            @endphp
+
+            @foreach ($bplaces as $place)
                 <div class="make-3D-space">
                     <div class="product-card">
                         <div id="product-front">
                             <div class="shadow"></div>
-                            <img class="product-image" src="https://images.pexels.com/photos/1078983/pexels-photo-1078983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+
+                            @php
+                            $thumbnail = App\Models\Image::where('pno', $place->id)
+                                ->where('name', 'like', '%ipi1%')
+                                ->first();
+                            @endphp
+                            <img class="product-image" src="{{ asset('images/'.$thumbnail->name) }}" alt="" />
                             <div class="image_overlay"></div>
-                            <div class="view_details">View details</div>
+                            <a href="/more-info/{{ $place->id }}" class="view_details">View details</a>
                             <div class="stats">
                                 <div class="stats-container">
-                                    <span class="product_price">Rs. 5987</span>
-                                    <span class="product_name">Heroic Villa</span>
-                                    <p>Best Place to stay calm</p>
+                                    <span class="product_price">Rs. {{ $place->price }}</span>
+                                    <span class="product_name">{{ $place->name }}</span>
+                                    <p>{{ $place->location }}</p>
 
                                     <div class="product-options">
-                                        <strong>Rooms</strong>
-                                        <span>Delux/Super Delux</span>
+                                        @if ($place->gc)
+                                            
+                                            <strong>Guest Capacty</strong>
+                                            <span>{{ $place->gc }}</span>
+                                        @endif
                                         <strong>Rating</strong>
                                         <div class="colors">
-                                            <div class="c-gold"><span></span></div>
-                                            <div class="c-gold"><span></span></div>
-                                            <div class="c-gold"><span></span></div>
-                                            <div class="c-white"><span></span></div>
-                                            <div class="c-white"><span></span></div>
+                                            @for ($i=0;$i<$place->ratings;$i++)
+                                                
+                                                <div class="c-gold"><span></span></div>
+                                            @endfor
+                                           
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-back">
+                        {{-- <div class="product-back">
                             <div class="shadow"></div>
                             <div class="carousel">
                                 <ul>
                                     <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large.png"
                                             alt="" /></li>
-                                    <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large2.png"
-                                            alt="" /></li>
-                                    <li><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large3.png"
-                                            alt="" /></li>
+                                    
                                 </ul>
                                 <div class="arrows-perspective">
                                     <div class="carouselPrev">
@@ -59,10 +69,10 @@
                                 <div class="cy"></div>
                                 <div class="cx"></div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
-            @endfor
+            @endforeach
 
         </div>
 
