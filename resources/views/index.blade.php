72427e3d61a6f980e31ff7fc59953383afc027b9
Sumeet Balwade <balwadesr@gmail.com>
2021-05-13 19:47:20 +0530

    whatsapp added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,8 +27,41 @@
 
     @include("components.footer")
 
+<style>
+/* for desktop */
+.whatsapp_float {
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:80px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+        font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+        z-index:100;
+}
 
- 
+.whatsapp-icon {
+	margin-top:16px;
+}
+/* for mobile
+@media screen and (max-width: 767px){
+     .whatsapp-icon {
+	 margin-top:10px;
+     }
+    .whatsapp_float {
+        width: 40px;
+        height: 40px;
+        bottom: 20px;
+        right: 10px;
+        font-size: 22px;
+    }
+} */
+</style>
+<a href="https://wa.me/+918888987679" class="whatsapp_float d-flex justify-content-center align-items-center" target="_blank"> <i class="fab fa-whatsapp"></i></a>
 
    
     {{-- <!-- gallery light box -->
